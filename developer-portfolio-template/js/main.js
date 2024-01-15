f10d736816f65dcfdbfc9afe0407e990611f25e3
Eric Pedreschi <ericpedreschi@gmail.com>
2024-01-15 01:02:41 -0500

    feat: :beers: Create main portfolio web
    refactor

---
+++ developer-portfolio-template/js/main.js
@@ -0,0 +1,142 @@
+(function ($) {
+    "use strict";
+    
+    // loader
+    var loader = function () {
+        setTimeout(function () {
+            if ($('#loader').length > 0) {
+                $('#loader').removeClass('show');
+            }
+        }, 1);
+    };
+    loader();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+    
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.navbar').addClass('nav-sticky');
+        } else {
+            $('.navbar').removeClass('nav-sticky');
+        }
+    });
+    
+    
+    // Smooth scrolling on the navbar links
+    $(".navbar-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 45
+            }, 1500, 'easeInOutExpo');
+            
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
+    
+    
+    // Typed Initiate
+    if ($('.hero .hero-text h2').length == 1) {
+        var typed_strings = $('.hero .hero-text .typed-text').text();
+        var typed = new Typed('.hero .hero-text h2', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+    }
+    
+    
+    // Skills
+    $('.skills').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+
+
+    // Testimonials carousel
+    $(".testimonials-carousel").owlCarousel({
+        center: true,
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: {
+            0:{
+                items:1
+            }
+        }
+    });
+    
+    
+    
+    // Portfolio filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+
+    $('#portfolio-filter li').on('click', function () {
+        $("#portfolio-filter li").removeClass('filter-active');
+        $(this).addClass('filter-active');
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+
+    // webs 
+
+    // on click in the web-1 id element
+    $("#web-1").on('click', function () {
+        // go to the html page
+        window.location.href = "./webs/coffee-shop-html-template/index.html";
+    });
+
+    // on click in the web-2 id element
+    $("#web-2").on('click', function () {
+        // go to the html page
+        window.location.href = "./webs/bootstrap-restaurant-template/index.html";
+    });
+
+    // on click in the web-3 id element
+    $("#web-3").on('click', function () {
+        // go to the html page
+        window.location.href = "./webs/food-website-template/index.html";
+    });
+
+    // on click in the web-4 id element
+    $("#web-4").on('click', function () {
+        // go to the html page
+        window.location.href = "./webs/ice-cream-shop-website-template/index.html";
+    });
+
+    // on click in the web-5 id element
+    $("#web-5").on('click', function () {
+        // go to the html page
+        window.location.href = "./webs/Italian-Cuisine/index.html";
+    });
+
+    
+})(jQuery);
+
